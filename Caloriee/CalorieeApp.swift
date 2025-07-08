198a69af8cc2aeecc9fcc0cded437c8d567e9ad9
BenHaefner <ibenroy2@gmail.com>
2025-07-08 18:16:17 -0400

    Initial Commit

---
+++ Caloriee/CalorieeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieeApp.swift
+//  Caloriee
+//
+//  Created by Ben Haefner on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CalorieeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalorieeTests/CalorieeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieeTests.swift
+//  CalorieeTests
+//
+//  Created by Ben Haefner on 7/8/25.
+//
+
+import Testing
+@testable import Caloriee
+
+struct CalorieeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
